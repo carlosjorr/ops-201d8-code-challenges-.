5ea57a587977db1a147f6f89386551eec2c9b8ea
carlosjorr <carlosjorr@outlook.com>
2023-05-02 20:53:46 +0000

    this is my 6 commit

---
+++ challenge07.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# CPU
+echo "===== CPU Information ====="
+lshw -class processor | grep -E 'product|vendor|physical id|bus info|width'
+
+# RAM
+echo "===== RAM Information ====="
+lshw -class memory | grep -E 'description|physical id|size'
+
+# Display Adapter
+echo "===== Display Adapter Information ====="
+lshw -class display | grep -E 'description|product|vendor|physical id|bus info|width|clock|capabilities|configuration|resources'
+
+# Network Adapter
+echo "===== Network Adapter Information ====="
+lshw -class network | grep -E 'description|product|vendor|physical id|bus info|logical name|version|serial|size|capacity|width|clock|capabilities|configuration|resources'
